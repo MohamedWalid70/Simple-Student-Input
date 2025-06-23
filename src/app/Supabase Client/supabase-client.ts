bcd4b2642bbcdd87cffc9a634115e13f291d48c5
Mohamed <Mohamed.walid.7001@gmail.com>
2025-06-23 06:35:23 +0300

    Making some changes

--- src/app/Supabase Client/supabase-client.ts
+++ src/app/Supabase Client/supabase-client.ts
@@ -1,6 +1,7 @@
 import {createClient} from "@supabase/supabase-js";
+import { environment } from "../../environments/environment.development";
 
 export const supabase = createClient(
-    "https://addizepkdbdpmdoduamj.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkZGl6ZXBrZGJkcG1kb2R1YW1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2Mjk1NzIsImV4cCI6MjA2NjIwNTU3Mn0.oPuIwmI4sNHV6mJ9Ntkrvm5Hz0FD6ci7Qv9Ve92YRO0"
+    environment.projectUrl,
+    environment.projectUrl
 ) 
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {    
+    projectUrl: "https://addizepkdbdpmdoduamj.supabase.co",
+    apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkZGl6ZXBrZGJkcG1kb2R1YW1qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA2Mjk1NzIsImV4cCI6MjA2NjIwNTU3Mn0.oPuIwmI4sNHV6mJ9Ntkrvm5Hz0FD6ci7Qv9Ve92YRO0"
+};
